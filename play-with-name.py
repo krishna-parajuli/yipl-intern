a6e5d034dabbe31d9ec043241555b76c0b8695a7
shaardul <krishna.parajuli2010@gmail.com>
2013-11-27 15:09:30 +0545

    python code

---
+++ play-with-name.py
@@ -0,0 +1,36 @@
+name = raw_input("enter your full name ")
+
+name = name.title()
+splitted = name.split()
+firstName = splitted[0]
+lastName = splitted[1]
+lenfName = len(firstName)
+lenlName = len(lastName)
+length = lenfName + lenlName
+lowfName = firstName.lower()
+lowlName = lastName.lower()
+hungarian = lowfName+lastName
+vowels = ['a','e','i','o','u']
+count = 0
+vowelless=''
+
+if lenfName == lenlName:
+	status = "Good One"
+else:
+	status = "Not bad though"
+
+for element in lowfName+lowlName:
+	if element in vowels:
+		count=count+1
+	else:
+		vowelless=vowelless+element
+
+print "Length (%s %s): %d" % (firstName,lastName,length)
+
+print "Status: %s" % status
+
+print "Hungarian: %s" % hungarian
+
+print "Vowels: %d" % count
+
+print "Vowelless: %s" % vowelless
